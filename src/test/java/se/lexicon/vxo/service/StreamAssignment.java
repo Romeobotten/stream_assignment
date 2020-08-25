8f52e2676d9ad40502a4b70efc11386a56c08cdd
Romeobotten <romeobot@hotmail.com>
2020-08-26 00:09:36 +0200

    Commit 1.0, Task 1-7 Done.

--- src/test/java/se/lexicon/vxo/service/StreamAssignment.java
+++ src/test/java/se/lexicon/vxo/service/StreamAssignment.java
@@ -31,71 +31,82 @@ public class StreamAssignment {
     public void task1(){
         List<Integer> integers = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
 
-        //Write code here
+        integers.stream().forEach(System.out::println);   // -OK
 
     }
 
     /**
-     * Turning people into a Stream count all members
+     * Turning people into a Stream count all members   -OK
      */
     @Test
     public void task2(){
         long amount = 0;
 
-        //Write code here
+        amount = people.stream().count();
 
-        assertEquals(10000, amount);
+    assertEquals(10000, amount);
     }
 
     /**
-     * Count all people that has Andersson as lastName.
+     * Count all people that has Andersson as lastName.   -OK
      */
     @Test
     public void task3(){
         long amount = 0;
         int expected = 90;
 
-        //Write code here
+        amount = people.stream().map(Person::getLastName)
+                .filter(p -> p.equals("Andersson"))
+                .count();
 
         assertEquals(expected, amount);
     }
 
     /**
-     * Extract a list of all female
+     * Extract a list of all female   -OK
      */
     @Test
     public void task4(){
         int expectedSize = 4988;
         List<Person> females = null;
 
-        //Write code here
+        females = people.stream()
+                .filter(f -> f.getGender().equals(Gender.FEMALE))
+                .collect(Collectors.toCollection(ArrayList::new));
+
 
         assertNotNull(females);
         assertEquals(expectedSize, females.size());
     }
 
     /**
-     * Extract a TreeSet with all birthDates
+     * Extract a TreeSet with all birthDates     -OK
      */
     @Test
     public void task5(){
         int expectedSize = 8882;
         Set<LocalDate> dates = null;
 
-        //Write code here
+        dates = people.stream().map(Person::getDateOfBirth)
+                .collect(Collectors.toCollection(TreeSet::new));
 
         assertNotNull(dates);
+        assertTrue(dates instanceof TreeSet);
         assertEquals(expectedSize, dates.size());
     }
 
     /**
-     * Extract an array of all people named "Erik"
+     * Extract an array of all people named "Erik"   -OK
      */
     @Test
     public void task6(){
         int expectedLength = 3;
 
         Person[] result = null;
+
+        result = people.stream() //.map(Person::getFirstName)
+                .filter(p -> p.getFirstName().equals("Erik"))
+                .toArray(Person[]::new);
 
         //Write code here
 
@@ -104,7 +115,7 @@ public class StreamAssignment {
     }
 
     /**
-     * Find a person that has id of 5436
+     * Find a person that has id of 5436   -OK
      */
     @Test
     public void task7(){
@@ -112,7 +123,9 @@ public class StreamAssignment {
 
         Optional<Person> optional = null;
 
-        //Write code here
+        optional = people.stream()
+                .filter(p -> p.getPersonId() == 5436)
+                .findFirst();
 
         assertNotNull(optional);
         assertTrue(optional.isPresent());
@@ -129,6 +142,8 @@ public class StreamAssignment {
         Optional<Person> optional = null;
 
         //Write code here
+
+        people.stream().findAny();
 
         assertNotNull(optional);
         assertEquals(expectedBirthDate, optional.get().getDateOfBirth());
